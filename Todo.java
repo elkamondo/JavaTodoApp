bffd200c6f3875705680bfbf2b398dcc37df12e0
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2018-01-26 16:19:41 +0000

    Add Todo class

---
+++ Todo.java
@@ -0,0 +1,79 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Todo implements Comparable<Todo> {
+
+  private String id;
+  private String name;
+  private boolean completed;
+  private LocalDate createdAt = LocalDate.now();
+
+  public Todo(String name) {
+    this.name = name;
+    this.id = String.valueOf(Math.abs(hashCode())).substring(0, 8);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void complete() {
+   this.completed = true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, completed, createdAt);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null) {
+      return false;
+    }
+
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    Todo todo = (Todo)obj;
+    return Objects.equals(id, todo.id);
+  }
+
+  @Override
+  public int compareTo(Todo other) {
+    return name.compareTo(other.name);
+  }
+
+  @Override
+  public String toString() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    String dateCreatedAt = createdAt.format(formatter);
+
+    return String.format(
+      "Todo={id=%s, name='%s', completed=%b, createdAt=%s}",
+      id, name, completed, dateCreatedAt
+    );
+  }
+}
