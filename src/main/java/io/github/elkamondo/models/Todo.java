adcc60390bcb48d3619370780a0511a1ab0f822f
Mohcine EL KASSIB <elkassib.mohcine@gmail.com>
2019-11-01 20:43:38 +0100

    Add time to todo and change printing format

--- src/main/java/io/github/elkamondo/models/Todo.java
+++ src/main/java/io/github/elkamondo/models/Todo.java
@@ -1,103 +1,98 @@
 package io.github.elkamondo.models;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Todo implements Comparable<Todo> {
 
-  private String id;
-  private String name;
-  private boolean completed;
-  private LocalDate createdAt = LocalDate.now();
+    private final String id;
+    private String name;
+    private boolean completed;
+    private LocalDateTime createdAt = LocalDateTime.now();
 
-  public Todo(String name) {
-    this.name = name;
-    this.id = generateId();
-  }
-
-  public Todo(String name, boolean completed, LocalDate createdAt) {
-    this.name = name;
-    this.completed = completed;
-    this.createdAt = createdAt;
-    this.id = generateId();
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public LocalDate getCreatedAt() {
-    return createdAt;
-  }
-
-  public String getFormattedDate() {
-    return createdAt.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-  }
-
-  public boolean isCompleted() {
-    return completed;
-  }
-
-  public void complete() {
-   this.completed = true;
-  }
-
-  @Override
-  public int hashCode() {
-    return 31 * Objects.hash(name, completed, createdAt);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    public Todo(String name) {
+        this.name = name;
+        this.id = generateId();
     }
 
-    if (obj == null) {
-      return false;
+    public Todo(String id, String name, boolean completed, LocalDateTime createdAt) {
+        this.name = name;
+        this.completed = completed;
+        this.createdAt = createdAt;
+        this.id = id;
     }
 
-    if (getClass() != obj.getClass()) {
-      return false;
+    public String getId() {
+        return id;
     }
 
-    Todo todo = (Todo)obj;
-    return Objects.equals(id, todo.id);
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  @Override
-  public int compareTo(Todo other) {
-    return name.compareTo(other.name);
-  }
+    public String getName() {
+        return name;
+    }
 
-  @Override
-  public String toString() {
-    return String.format(
-      "Todo={id=%s, name='%s', completed=%b, createdAt=%s}",
-      id, name, completed, getFormattedDate()
-    );
-  }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-  public void print() {
-    System.out.println(
-      String.format(
-        "  %s | %-15s | %-9b | %s",
-        id, name, completed, getFormattedDate()
-      )
-    );
-  }
+    public String getFormattedDate() {
+        return createdAt.format(DateTimeFormatter.ofPattern("dd/MM/yyyy H:m:s"));
+    }
 
-  private String generateId() {
-    String hash = Integer.toString(Math.abs(hashCode()));
-    return hash.substring(Math.max(0, hash.length() - 5));
-  }
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Objects.hash(id, name, completed, createdAt);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final Todo todo = (Todo)obj;
+        return Objects.equals(id, todo.id)
+                && Objects.equals(name, todo.name)
+                && Objects.equals(completed, todo.completed)
+                && Objects.equals(createdAt, todo.createdAt);
+    }
+
+    @Override
+    public int compareTo(Todo other) {
+        return name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Todo={id=%s, name='%s', completed=%b, createdAt=%s}",
+                id, name, completed, getFormattedDate()
+        );
+    }
+
+    private String generateId() {
+        String hash = Integer.toString(Math.abs(hashCode()));
+        return hash.substring(Math.max(0, hash.length() - 5));
+    }
+
 }
--- src/main/java/io/github/elkamondo/models/TodoList.java
+++ src/main/java/io/github/elkamondo/models/TodoList.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Comparator;
@@ -18,157 +18,163 @@ import java.util.stream.Collectors;
 
 public class TodoList {
 
-  public static Comparator<Todo> BY_NAME_THEN_DATE =
-    Comparator.comparing(Todo::getCreatedAt)
-              .thenComparing(Todo::getName);
-  private Set<Todo> todos = new TreeSet<>(BY_NAME_THEN_DATE);
+    private static Comparator<Todo> BY_NAME_THEN_DATE =
+            Comparator.comparing(Todo::getCreatedAt)
+                      .thenComparing(Todo::getName);
 
-  public TodoList() {
-  }
+    private Set<Todo> todos = new TreeSet<>(BY_NAME_THEN_DATE);
 
-  public TodoList(Collection<? extends Todo> collection) {
-    if (collection != null) {
-      todos.addAll(collection);
-    }
-  }
+    public TodoList() {}
 
-  public Set<Todo> getTodos() {
-    return todos;
-  }
-
-  public boolean isEmpty() {
-    return todos.isEmpty();
-  }
-
-  @Override
-  public String toString() {
-    return todos.toString();
-  }
-
-  public boolean addTodo(Todo todo) {
-    if (todo == null) {
-      return false;
+    public TodoList(Collection<? extends Todo> collection) {
+        if (collection != null) {
+            todos.addAll(collection);
+        }
     }
 
-    return todos.add(todo);
-  }
-
-  public boolean completeTodo(String todoId) {
-    if (todoId == null) {
-      return false;
+    public Set<Todo> getTodos() {
+        return todos;
     }
 
-    Todo completedTodo = todos.stream()
-      .filter(todo -> todoId.equals(todo.getId()))
-      .findFirst()
-      .orElse(new Todo("n/a"));
-
-    if (!completedTodo.getName().equalsIgnoreCase("n/a")) {
-      completedTodo.complete();
-      return true;
+    public boolean isEmpty() {
+        return todos.isEmpty();
     }
 
-    return false;
-  }
-
-  public boolean removeTodo(String todoId) {
-    if (todoId == null) {
-      return false;
+    @Override
+    public String toString() {
+        return todos.toString();
     }
 
-    return todos.removeIf(todo -> todoId.equals(todo.getId()));
-  }
+    public boolean addTodo(Todo todo) {
+        if (todo == null) {
+            return false;
+        }
 
-  public void showAllTodos() {
-    if (todos.isEmpty()) {
-      System.out.println("No todos.");
-      return;
+        return todos.add(todo);
     }
 
-    printTableHeader();
-    todos.forEach(Todo::print);
-  }
+    public boolean completeTodo(String todoId) {
+        if (todoId == null) {
+            return false;
+        }
 
-  public void showActiveTodos() {
-    Predicate<Todo> isCompleted = Todo::isCompleted;
-    TreeSet<Todo> activeTodos = todos.stream()
-      .filter(isCompleted.negate())
-      .collect(Collectors.toCollection(() -> new TreeSet<>(BY_NAME_THEN_DATE)));
+        Todo completedTodo = todos.stream()
+                .filter(todo -> todoId.equals(todo.getId()))
+                .findFirst()
+                .orElse(new Todo("n/a"));
 
-    if (activeTodos.isEmpty()) {
-      System.out.println("No active todos.");
-    } else {
-      printTableHeader();
-      activeTodos.forEach(Todo::print);
-    }
-  }
+        if (!completedTodo.getName().equalsIgnoreCase("n/a")) {
+            completedTodo.complete();
+            return true;
+        }
 
-  public void showCompletedTodos() {
-    TreeSet<Todo> completedTodos = todos.stream()
-      .filter(Todo::isCompleted)
-      .collect(Collectors.toCollection(() -> new TreeSet<>(BY_NAME_THEN_DATE)));
-
-    if (completedTodos.isEmpty()) {
-      System.out.println("No completed todos.");
-    } else {
-      printTableHeader();
-      completedTodos.forEach(Todo::print);
-    }
-  }
-
-  public void saveTodos() throws IOException {
-    Path path = Paths.get("todos.backup");
-    if (Files.notExists(path)) {
-      Files.createFile(path);
+        return false;
     }
 
-    String content =
-      todos.stream()
-          .map(todo ->
-            String.format(
-              "%s | %b | %s",
-              todo.getName(), todo.isCompleted(), todo.getFormattedDate()
-            )
-          )
-          .collect(Collectors.joining("\n"));
+    public boolean removeTodo(String todoId) {
+        if (todoId == null) {
+            return false;
+        }
 
-    Files.write(path, content.getBytes());
-    System.out.println("Your todos has been saved successfully.");
-  }
-
-  public void retrieveStoredTodos() {
-    Path path = Paths.get("todos.backup");
-    if (Files.exists(path)) {
-      try {
-        TodoList persistedData = new TodoList();
-        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-        lines.stream()
-             .forEach(line -> {
-                StringTokenizer st = new StringTokenizer(line, "|");
-                while (st.hasMoreTokens()) {
-                  String name = st.nextToken().trim();
-                  Boolean completed = Boolean.valueOf(st.nextToken().trim());
-                  DateTimeFormatter formatter =
-                    DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                  LocalDate createdAt = LocalDate.parse(
-                    st.nextToken().trim(),
-                    formatter
-                  );
-
-                  persistedData.addTodo(new Todo(name, completed, createdAt));
-               }
-             });
-        todos.addAll(persistedData.getTodos());
-      } catch (IOException e) {
-      }
+        return todos.removeIf(todo -> todoId.equals(todo.getId()));
     }
-  }
 
-  private void printTableHeader() {
-    System.out.printf(
-     "  %-5s | %-15s | %-5s | %s\n",
-     "id", "name", "completed", "createdAt"
-    );
-    System.out.println("--------------------------------------------------");
-  }
+    public void showAllTodos() {
+        if (todos.isEmpty()) {
+            System.out.println("No todos.");
+            return;
+        }
+
+        printTodos(todos);
+    }
+
+    public void showActiveTodos() {
+        Predicate<Todo> isCompleted = Todo::isCompleted;
+        TreeSet<Todo> activeTodos = todos.stream()
+                .filter(isCompleted.negate())
+                .collect(Collectors.toCollection(() -> new TreeSet<>(BY_NAME_THEN_DATE)));
+
+        if (activeTodos.isEmpty()) {
+            System.out.println("No active todos.");
+        } else {
+            printTodos(activeTodos);
+        }
+    }
+
+    public void showCompletedTodos() {
+        TreeSet<Todo> completedTodos = todos.stream()
+                .filter(Todo::isCompleted)
+                .collect(Collectors.toCollection(() -> new TreeSet<>(BY_NAME_THEN_DATE)));
+
+        if (completedTodos.isEmpty()) {
+            System.out.println("No completed todos.");
+        } else {
+            printTodos(completedTodos);
+        }
+    }
+
+    public void saveTodos() throws IOException {
+        Path path = Paths.get("todos.backup");
+        if (Files.notExists(path)) {
+            Files.createFile(path);
+        }
+
+        String content =
+                todos.stream()
+                        .map(todo ->
+                                String.format(
+                                        "%s | %s | %b | %s",
+                                        todo.getId(), todo.getName(), todo.isCompleted(), todo.getFormattedDate()
+                                )
+                        )
+                        .collect(Collectors.joining("\n"));
+
+        Files.write(path, content.getBytes());
+        System.out.println("Your todos has been saved successfully.");
+    }
+
+    public void retrieveStoredTodos() {
+        Path path = Paths.get("todos.backup");
+        if (Files.exists(path)) {
+            try {
+                TodoList persistedData = new TodoList();
+                List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+                lines.stream()
+                        .forEach(line -> {
+                            StringTokenizer st = new StringTokenizer(line, "|");
+                            while (st.hasMoreTokens()) {
+                                String id = st.nextToken().trim();
+                                String name = st.nextToken().trim();
+                                Boolean completed = Boolean.valueOf(st.nextToken().trim());
+                                DateTimeFormatter formatter =
+                                        DateTimeFormatter.ofPattern("dd/MM/yyyy H:m:s");
+                                LocalDateTime createdAt = LocalDateTime.parse(
+                                        st.nextToken().trim(),
+                                        formatter
+                                );
+
+                                persistedData.addTodo(new Todo(id, name, completed, createdAt));
+                            }
+                        });
+                todos.addAll(persistedData.getTodos());
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private void printTodos(Collection<? extends Todo> todos) {
+        printTableHeader();
+        todos.forEach(todo ->
+                System.out.printf("| %s | %-30s | %-9b | %s |%n",
+                        todo.getId(), todo.getName(), todo.isCompleted(), todo.getFormattedDate())
+        );
+        System.out.println("+-------+--------------------------------+-----------+--------------------+");
+    }
+
+    private void printTableHeader() {
+        System.out.println("+-------+--------------------------------+-----------+--------------------+");
+        System.out.printf("| %-5s | %-30s | %-5s | %-18s |%n", "id", "name", "completed", "createdAt");
+        System.out.println("+-------+--------------------------------+-----------+--------------------+");
+    }
+
 }
